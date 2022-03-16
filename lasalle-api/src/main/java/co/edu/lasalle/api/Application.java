159269fcb1e8b946d8576927079ca310ac0157d2
Julian Lopez <stivenslop@hotmail.com>
2022-03-15 19:54:38 -0500

    Docs: Agregar swagger para documentar la APÍ

--- lasalle-api/src/main/java/co/edu/lasalle/api/Application.java
+++ lasalle-api/src/main/java/co/edu/lasalle/api/Application.java
@@ -7,19 +7,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Application
-{
-	public static void main(String[] args)
-	{
-		SpringApplication.run(Application.class, args);
-	}
+public class Application {
 
-	@Bean
-  public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder)
-  {
-    return restTemplateBuilder
-      .setConnectTimeout((int)30e3)
-      .setReadTimeout((int)30e3)
-      .build();
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout((int) 30e3)
+                .setReadTimeout((int) 30e3)
+                .build();
+    }
 }
